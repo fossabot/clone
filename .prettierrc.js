a03a6e4e7d6f9377e9196f40bf8298710d23667b
Egor Efimenko <79161404+EgorEfimenko@users.noreply.github.com>
2022-03-03 13:54:36 +0600

    init

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: false,
+  bracketSpacing: true,
+  tabWidth: 2,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
